0b16c9922bd7592d6f4d8589511baf39cb9225e0
gprok <gprok60@gmail.com>
2024-02-23 15:29:19 +0200

    week 2 exercises

---
+++ src/week_02/Counter.java
@@ -0,0 +1,9 @@
+package week_02;
+
+public class Counter {
+    public static void main(String[] args) {
+        for(int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
